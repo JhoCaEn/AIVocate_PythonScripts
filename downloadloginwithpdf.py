565ec8db58b13252297a0f9ab6b135c34e366c67
Jhonny Camacho E <128161166+JhoCaEn@users.noreply.github.com>
2023-10-26 12:53:53 -0400

    last

--- downloadloginwithpdf.py
+++ downloadloginwithpdf.py
@@ -1,35 +1,59 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+import openai
+import os
+import PyPDF2
+import random
+import string
+from google.cloud import storage
+from io import BytesIO
+import requests
+import firebase_admin
+# from firebase_admin import firestore
+from google.cloud import firestore
 
+def access_to_case(page):
+    try:
+        page.goto("https://oficinajudicialvirtual.pjud.cl/home/index.php")
+        page.get_by_role("button", name="Todos los servicios").click()
+        page.get_by_role("button", name="Todos los servicios").click()
+        page.locator("#myDropdown").get_by_role("link", name="Clave Única").click()
+        page.get_by_label("Ingresa tu RUN").click()
+        page.get_by_label("Ingresa tu RUN").fill("13.597.575-3")
+        page.get_by_label("Ingresa tu ClaveÚnica").fill("Bularz113!")
+        page.get_by_label("Ingresa", exact=True).click()
+    #    page.goto("https://oficinajudicialvirtual.pjud.cl/indexN.php")
+        page.get_by_role("link", name="Mis Causas").click()
+        page.get_by_role("row", name="1842-2015 SOCIEDAD DE SERVICIOS DE ALIMENTACION S A/JUNTA NACIONAL DE AUXILIO ESCOLAR Y BECAS 04/02/2015 Fallada Corte Suprema").get_by_role("link").click()
+        return True
+    except Exception:
+        return False
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, timeout=10000)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://oficinajudicialvirtual.pjud.cl/home/index.php")
-    page.get_by_role("button", name="Todos los servicios").click()
-    page.get_by_role("button", name="Todos los servicios").click()
-    page.locator("#myDropdown").get_by_role("link", name="Clave Única").click()
-    page.get_by_label("Ingresa tu RUN").click()
-    page.get_by_label("Ingresa tu RUN").fill("13.597.575-3")
-    page.get_by_label("Ingresa tu ClaveÚnica").fill("Bularz113!")
-    page.get_by_label("Ingresa", exact=True).click()
-#    page.goto("https://oficinajudicialvirtual.pjud.cl/indexN.php")
-    page.get_by_role("link", name="Mis Causas").click()
-    page.get_by_role("row", name="1842-2015 SOCIEDAD DE SERVICIOS DE ALIMENTACION S A/JUNTA NACIONAL DE AUXILIO ESCOLAR Y BECAS 04/02/2015 Fallada Corte Suprema").get_by_role("link").click()
+    accessed = access_to_case(page)
+    while(accessed == False):
+        accessed = access_to_case(page)
 #    with page.expect_popup() as page1_info:
 #        page.get_by_role("row", name="2015 04/02/2015 Otro Tramite CERTIFICADO DE INGRESO Unidad Ingreso Bloqueado").get_by_role("link").click()
 #    page1 = page1_info.value
 
-#implementacion de descarga de un pop up
-#    with page.expect_popup() as page1_info:
-#        new_page = context.expect_page()
+# implementacion de descarga de un pop up
+    with page.expect_popup() as page1_info:
+        new_page = context.expect_page()
+        page.locator('#contenedorEbook').get_by_role("link").click()
 #        page.get_by_role("link", name="Descargar Certificado").click()
-#        new_page = context.wait_for_event("page")
-#        pdf_url = new_page.url
-#        print(pdf_url)
-#        response = requests.get(pdf_url)
-#        with open('output.pdf', 'wb') as f:
-#            f.write(response.content)
+        new_page = context.wait_for_event("page")
+        pdf_url = new_page.url
+        print(pdf_url)
+        response = requests.get(pdf_url)
+#        storage_client = storage.Client()
+#        bucket = storage_client.bucket('projectonepdflawyers')
+#        blob = bucket.blob("test_downloadpdf.pdf") 
+#        blob.upload_from_file(response.content,rewind=True)
+        with open('output.pdf', 'wb') as f:
+            f.write(response.content)
 
 #codigo para subitlo al bukcet
 #        storage_client = storage.Client()
@@ -46,5 +70,10 @@ def run(playwright: Playwright) -> None:
     browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+
+def main():
+    with sync_playwright() as p:
+        run(p)
+
+if __name__ == "__main__":
+    main()
--- uploadgpttext.py
+++ uploadgpttext.py
@@ -9,50 +9,76 @@ from google.cloud import storage
 from io import BytesIO
 import requests
 import firebase_admin
-#from firebase_admin import firestore
+# from firebase_admin import firestore
 from google.cloud import firestore
 
+def access_to_case(page):
+    try:
+        page.goto("https://oficinajudicialvirtual.pjud.cl/home/index.php")
+        page.get_by_role("button", name="Todos los servicios").click()
+        page.locator("#myDropdown").get_by_role("link", name="Clave Única").click()
+        page.get_by_label("Ingresa tu RUN").click()
+        page.get_by_label("Ingresa tu RUN").fill("13.597.575-3")
+        page.get_by_label("Ingresa tu ClaveÚnica").click()
+        page.get_by_label("Ingresa tu ClaveÚnica").fill("Bularz113!")
+        page.get_by_label("Ingresa", exact=True).click()
+        return True
+    except Exception:
+        return False
+def first_step(page1):
+    try:
+        page1.get_by_text("13.597.575-3").click()
+        page1.get_by_text("Ingresar Demanda/Recurso").click()
+        page1.locator(
+            '''select[data-bind="options: competencias, optionsText: 'nombre', optionsCaption: 'Seleccione Competencia', value : competencia"]''').select_option('Garantia') 
+        page1.locator("#s2id_select-asientoCorte").click()#Corte
+        page1.get_by_text('C.A. de Santiago.').click()
+        page1.locator("#s2id_select-tribunales").click()#Tribunal
+        page1.get_by_text('5º Juzgado de Garantía de Santiago').click()
+        return True
+    except Exception:
+        return False
+def second_step(page1):
+    try:
+        page1.get_by_text('Seleccione Procedimiento').click()
+        page1.get_by_text('Ordinario').click()
+
+        page1.get_by_text('Seleccione Forma Inicio').click()
+        page1.get_by_text('Querella').click()
+
+        page1.locator("xpath=////div[@class='d-none d-sm-block col-sm-3 col-lg-2']//div[@class='form-group']//input[@type='text']").fill("538")
+        page1.get_by_text("Agregar").click()
+        return True
+    except Exception:
+        return False
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, timeout=10000)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://oficinajudicialvirtual.pjud.cl/home/index.php")
-    page.get_by_role("button", name="Todos los servicios").click()
-    page.locator("#myDropdown").get_by_role("link", name="Clave Única").click()
-    page.get_by_label("Ingresa tu RUN").click()
-    page.get_by_label("Ingresa tu RUN").fill("13.597.575-3")
-    page.get_by_label("Ingresa tu ClaveÚnica").click()
-    page.get_by_label("Ingresa tu ClaveÚnica").fill("Bularz113!")
-    page.get_by_label("Ingresa", exact=True).click()
+    accessed = access_to_case(page)
+    while(accessed == False):
+        accessed = access_to_case(page)
 #    page.goto("https://oficinajudicialvirtual.pjud.cl/indexN.php")
 #   ajustar  a pagina con token que genral
     with page.expect_popup() as page1_info:
         page.get_by_role("link", name="Ing. Demandas y Escritos").click()
     page1 = page1_info.value
-#    page1.goto("https://ojv.pjud.cl/kpitec-ojv-web/index?auth-token=NDVlCvpQyvkxnetgSVpIbh1jz2S7JGkDYgB8ht3LZ2g%3D&version=1.1#popup/roles")
-    page1.get_by_text("13.597.575-3").click()
-    page1.get_by_text("Ingresar Demanda/Recurso").click()
-    page1.get_by_role("class",name="form-control").click()
-    page1.get_by_role("link", name="VICTOR IVAN VERGARA BULARZ").click()
-    page1.get_by_role("option", name="VICTOR IVAN VERGARA BULARZ").click()
-    page1.locator("#select2-chosen-27").click()
-    page1.get_by_text('C.A. de Santiago.').click()
-    page1.get_by_text('5º Juzgado de Garantía de Santiago').click()
 
+    #recuadro uno ingreso demanda y recursos
+    accessed = first_step(page1)
+    while(accessed == False):
+        print("first step")
+        accessed = first_step(page1)
 
     #recuadro dos ingreso demanda
-    page1.locator('#select2-chosen-210').click()
-    page1.get_by_text('Ordinario').click()
-
-    page1.locator('select2-chosen-212').click()
-    page1.get_by_text('Ordinario').click()
-
-    page1.locator("xpath=////div[@class='d-none d-sm-block col-sm-3 col-lg-2']//div[@class='form-group']//input[@type='text']").fill("538")
-    page1.get_by_text("Agregar").click()
+    accessed = second_step(page1)
+    while(accessed == False):
+        print("second step")
+        accessed = second_step(page1)
 
     # ---------------------
-    #litigantes QTE
+    # litigantes QTE
     page1.locator('#s2id_autogen151').click()
     page1.locator('#select2-result-label-164').click()
 
@@ -61,13 +87,13 @@ def run(playwright: Playwright) -> None:
 
     page1.get_by_text("Agregar Litigante").click()
 
-    #querellado QDo contra quier resulter responsables
+    # querellado QDo contra quier resulter responsables
     page1.get_by_text(" Contra quienes resulten responsables").click()
     page1.locator('#select2-chosen-152').click()
     page1.locator('#select2-result-label-177').click()
     page1.get_by_text("Agregar Litigante").click()
 
-    #agregar abogadado ABO
+    # agregar abogadado ABO
     page1.locator('#s2id_autogen151').click()
     page1.locator('#select2-result-label-179').click()
 
@@ -76,25 +102,26 @@ def run(playwright: Playwright) -> None:
     page1.get_by_text("Agregar Litigante").click()
 
     page1.get_by_text(" Ingresar").click()
-    #----------------
-    #Subir documento se debe usar with para el popou
-    
+    # ----------------
+    # Subir documento se debe usar with para el popou
+
     with page1.expect_popup() as page1_info:
-        current_directory= os.getcwd()
+        current_directory = os.getcwd()
         file_path = os.path.join(current_directory, 'elfile.pdf')
         page.get_by_text('Adjuntar').set_input_files([file_path])
         page.wait_for_timeout(4000)
 
         page.get_by_text("Cerrar y Continuar").click
-    #---------------
+    # ---------------
     context.close()
     browser.close()
 
-#try:
-with sync_playwright() as playwright:
-        run(playwright)
 
-#except Exception:
+# try:
+with sync_playwright() as playwright:
+    run(playwright)
+
+# except Exception:
 #    db = firestore.Client()
 #    doc_ref = db.collection('deverrorlog').document('testone')
 #    doc_ref.update({"isfail":True})
